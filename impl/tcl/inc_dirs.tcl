27827832f75865bba1b8d4dbac0680deeb342fd3
Luca Hanel <lhanel@protonmail.com>
2020-08-31 19:13:35 +0200

    removed unnessessary defines

--- impl/tcl/inc_dirs.tcl
+++ impl/tcl/inc_dirs.tcl
@@ -1,8 +1,6 @@
 set_property include_dirs {
     ../rtl/includes/ \
     ../ips/debug_module/includes/ \
-    ../ips/gpio_module/inc/ \
-    ../ips/timer_module/includes/ \
     ../ips/wishbone/includes/ \    
     ../ips/apb/inc/ \
 } [current_fileset]
